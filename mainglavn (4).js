63d0a8ea2607781916b20333e031a132d3e31aec
izmnastya18 <51162228+izmnastya18@users.noreply.github.com>
2019-12-19 22:31:01 +0300

    Add files via upload

--- mainglavn (4).js
+++ mainglavn (4).js
@@ -1,4 +1,4 @@
-const dataTasks = [
+const dataTasks1lvl = [
 	{
 		question: 'Hello',
 		answer: ['Прыгать', 'Пока', 'Море', 'Бегать', 'Привет'],
@@ -11,7 +11,7 @@ const dataTasks = [
 	},
 	{
 		question: 'Set up',
-		answer: ['Установить', 'Примерить', 'Набраться сил', 'Разбогатеть',"Бегать"],
+		answer: ['Установить', 'Примерить', 'Набраться сил', 'Разбогатеть', "Бегать"],
 		trueAnswer: 'Установить'
 	},
 	{
@@ -31,107 +31,161 @@ const dataTasks = [
 	}
 ];
 
+const dataTasks2lvl = [
+	{
+		question: 'sixteen',
+		trueAnswer: 'Шестнадцать',
+	},
+	{
+		question: 'veteran',
+		trueAnswer: 'Ветеран'
+	},
+	{
+		question: 'fool',
+		trueAnswer: 'дурак'
+	},
+	{
+		question: 'Cuba',
+		trueAnswer: 'Куба'
+	},
+	{
+		question: 'panic',
+		trueAnswer: 'паника'
+	},
+	{
+		question: 'calendar',
+		trueAnswer: 'календарь',
+	}
+];
 
 let play = false; // идёт ли тест
-let count = dataTasks.length; // Всего вопросов
-let timeQuestions = 0;
+let count = dataTasks1lvl.length; // Всего вопросов
+let time = 0;
 let countAnswer = 0;
-let currentQuestions = null// dataTasks[countAnswer];// текущий вопрос
-// Номер текущего вопроса
+let currentQuestions = 0;// dataTasks[countAnswer];// Номер текущего вопроса
 let startButton = document.getElementById('start');
-let pQuestion = document.querySelector('p');
+let pQuestion = document.querySelector('#word');
 let countTrueAnswers = 0;
-let countPraviln = document.getElementsByClassName('answer').length; 
+let countPraviln = document.getElementsByClassName('answer').length;
 let buttonsAnswer;
 let buttonText;//содержимое кнопки
-
-
-// console.log(dataTasks[0].answer.length);
-
-
-
-
-
+let kotlet = dataTasks1lvl[5].answer[2];
+let res =  document.getElementById('result');
+console.log(kotlet);
 
 //////////// Основая логика теста начало!
 startButton.onclick = start; //ЛОВИМ КЛИК НА НАЧАЛО ТРЕНАЖЁРА
 
-function test() {
-	if (play == true ) {
+function test(){
+	if (play == true){
 		let answerButton = document.getElementsByClassName('answer');
 		for (let index = 0; index < answerButton.length; index++) {
-			let buttons = answerButton[index].addEventListener('click', function (event) {
-				event.preventDefault(); // Чтобы только на кликнутой кнопке сработало
-				buttonText = this.textContent;//содержимое кнопки
-				if(buttonText == dataTasks[countAnswer].trueAnswer) {
-					console.log('rightAnswer');
-					countTrueAnswers++;
-					++countAnswer; 
-					 if (countAnswer != count){
-						currentQuestions = dataTasks[countAnswer];
-						generateNewTask(currentQuestions); //каждый раз вызываем чтобы менялись 
-					 }
-					 else endTest();
-				}
-				else{
-					console.log('errror');
-				}
-			})
-
+			let buttons = answerButton[index].addEventListener('click', level1);
 		}
-
 	}
-	//else{
-		
-		// Тут будт запускаться ф-и конца теста
-		//document.getElementById('question').innerHTML=document.getElementById('question').innerHTML = dataTasks.aswer  + (currentAnswer+1) + '/' + countAnswer;
-		// currentAnswer = dataTasks[count]
-		// generateTask(currentAnswer); 
-		//	}
-		
-		//////////// Основая логика теста заканчивается
-		
-	}
-	
-console.log(countTrueAnswers);
-
-///////////// Вспомогательные ф-и
-// let intervalID = setInterval(sec, 1000);
-// функция для вывода результата
-function generateNewTask(currentQuestions) {
-	console.log(currentQuestions.answer.length)
-	document.querySelector('p').innerHTML = currentQuestions.question; //выводится слово,которое нужно переводить
-		let buts = document.querySelectorAll('button')
-	for (let i = 0; i < currentQuestions.answer.length; i++) { 
-		buts[i].innerHTML = currentQuestions.answer[i];
-		console.log(currentQuestions.answer[i])
-	}
-	//document.getElementById('result').textContent = currentQuestions.question;
 }
 
+
+function level1(event){
+		event.preventDefault(); // Чтобы только на кликнутой кнопке сработало
+		buttonText = this.textContent;//содержимое кнопки
+		if (buttonText == dataTasks1lvl[countAnswer].trueAnswer && countAnswer < dataTasks1lvl.length) {
+			countTrueAnswers++;
+			++countAnswer;
+			console.log(countAnswer);
+			if (countAnswer < count){
+				currentQuestions = dataTasks1lvl[countAnswer];
+				generateNewTask(currentQuestions); //каждый раз вызываем чтобы менялись 	
+			}
+			else{
+				endTest();
+				endTimer();
+			}
+		}
+		else{
+			countAnswer++;
+			console.log(countAnswer);
+			currentQuestions = dataTasks1lvl[countAnswer];
+			generateNewTask(currentQuestions);
+}		
+}
+
+
+//функция второго уровня
+function level2(){
+	let inputs = document.createElement('input');
+	document.body.appendChild(inputs);
+	setTimeout(()=>res.remove(),2000);
+	let resultStr = `Всё правильно!Уровень 2`;
+	document.getElementById('result').innerHTML = resultStr;
+	document.querySelector('div').style.display = 'block';
+	document.getElementById('word').style.display = 'block';
+	for(let i=0; i < dataTasks2lvl.length;i++){
+		pQuestion.innerHTML = dataTasks2lvl[i].question;
+		console.log(dataTasks2lvl[i].question);
+	}
+}
+
+
+///////////// Вспомогательные ф-и
 // функция начала теста
 function start() { // Ждём когда нажмут кнопку старт и запускаем тест 
 	play = true; //меняем на true
-	startButton.style.display = 'none'; //скрываем кнопку НАЧАТЬ
-	generateTaskBlock(dataTasks,countAnswer); //запускаем функцию с countAnswer, которая изначально 0
+	let intervalID = setInterval(sec, 1000);
+	startButton.style.display = 'none';//скрываем кнопку НАЧАТЬ
+	generateTaskBlock(dataTasks1lvl, countAnswer); //запускаем функцию с countAnswer, которая изначально 0
 	test(); //запускаем основную функцию теста
 }
 
-// функция создания кнопок теста, вывода слова, которое нужно переводить
-function generateTaskBlock(dataTask,counts) { 
-	for (let i = 0; i < dataTask[counts].answer.length; i++) { //пока i каждый раз меньше длины количества вопросов у каждого обьекта
+//функция создания кнопок теста 1 уровня, вывода слова, которое нужно переводить
+function generateTaskBlock(dataTask1lvl, counts) {
+	for (let i = 0; i < dataTask1lvl[counts].answer.length; i++) { //пока i каждый раз меньше длины количества вопросов у каждого обьекта
 		buttonsAnswer = document.createElement('button'); //создаём кнопки 
 		document.body.appendChild(buttonsAnswer).classList.add('answer'); //создаём класс у кнопок при их создании
 		divButtons = document.querySelector('div'); //находим уже созданный div
 		divButtons.insertAdjacentElement("afterbegin", buttonsAnswer); //в контейнер помещаются созданные кнопки
-		pQuestion.innerHTML = dataTask[counts].question; //выводится слово,которое нужно переводить
-		buttonsAnswer.innerHTML = dataTasks[counts].answer[i]; //в кнопку записывается значение из массива всех ответов вместе с правильным
-		console.log(dataTask[counts].answer[i]);
+		pQuestion.innerHTML = dataTask1lvl[counts].question; //выводится слово,которое нужно переводить
+		buttonsAnswer.innerHTML = dataTasks1lvl[counts].answer[i]; //в кнопку записывается значение из массива всех ответов вместе с правильным
+		console.log(dataTask1lvl[counts].answer[i]);
 	}
 }
-function endTest() {
-	document.querySelector('div').remove();
-	document.querySelector('p').remove();
-	p=document.getElementById('result'); //выводим нужный результат
+//функция для вывода следующих слов
+function generateNewTask(currentQuestions) {
+	document.getElementById('word').innerHTML = currentQuestions.question; //выводится слово,которое нужно переводить
+	let buts = document.getElementsByClassName('answer');
+	for (let i = 0; i < currentQuestions.answer.length; i++) {
+		buts[i].innerHTML = currentQuestions.answer[i];
+	}	
 }
-
+//конец тренажера
+function endTest() {
+	document.querySelector('div').style.display = 'none';
+	document.getElementById('word').style.display = 'none';
+	let resultStr = `Правильных ответов: ${countTrueAnswers}  из  ${countAnswer} `;
+	document.getElementById('result').innerHTML = resultStr;
+	return;
+}
+function endTimer(){
+	document.querySelector('#time').style.display = 'none';
+}
+//функция таймера
+function sec() {
+	if(time <= 12){
+	time++;	
+	document.getElementById('time').innerText = `Время: ${time}  сек`;
+	}
+	else{
+		let stop  = `Время вышло!`;
+		res.innerHTML = stop ; 
+		endTimer();
+	}
+}
+//функция создания второго теста
+function generateTaskBlockFor_2Level(dataTask2lvl, counts) {
+	inputAnswer = document.createElement('input'); //создаём кнопки 
+	for (let i = 0; i < dataTask2lvl.length; i++) {
+		document.body.appendChild(inputAnswer).classList.add('answer'); //создаём класс у кнопок при их создании
+		divButtons = document.querySelector('div'); //находим уже созданный div
+		pQuestion.innerHTML = dataTask2lvl[0].question; //выводится слово,которое нужно переводить
+	}
+}
